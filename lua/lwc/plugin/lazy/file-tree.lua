3fb7131ae80649e3be2f587c7c436911e826bcbe
Charlie Lee <c050820021a@gmail.com>
2024-10-10 08:36:39 +0800

    not sure what it does but lets test the commit

--- lua/lwc/plugin/lazy/file-tree.lua
+++ lua/lwc/plugin/lazy/file-tree.lua
@@ -3,6 +3,7 @@ return {
     config = function()
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
+        vim.opt.termguicolors = true
         require("nvim-tree").setup({
             disable_netrw = true,
             hijack_cursor = true,
