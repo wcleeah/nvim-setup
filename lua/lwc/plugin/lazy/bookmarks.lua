6d0c4a93cdad5b58f86c530003676a8961655381
charlie <charlie@choco-up.com>
2024-10-14 17:48:31 +0800

    feat: remove bookmark

--- lua/lwc/plugin/lazy/bookmarks.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"LintaoAmons/bookmarks.nvim",
-	config = function()
-		vim.keymap.set({ "n", "v" }, "<leader>bm", "<cmd>BookmarksMark<cr>", { desc = "Mark current line into active BookmarkList." })
-		vim.keymap.set({ "n", "v" }, "<leader>bo", "<cmd>BookmarksGoto<cr>", { desc = "Go to bookmark at current active BookmarkList" })
-		vim.keymap.set({ "n", "v" }, "<leader>ba", "<cmd>BookmarksTree<cr>", { desc = "Display all bookmarks with tree-view, and use cut, paste, create folder to edit the tree." })
-	end
-}
